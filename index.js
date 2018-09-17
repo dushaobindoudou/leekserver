43e8767f889622f77fe2923232a345d00dd99e95
dushaobin <dushaobin@we.com>
2018-09-17 17:52:54 +0800

    更新代码 测试自动更新

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Koa = require('koa');
 const app = new Koa();
 
 app.use(async function(ctx) {
-    ctx.body = 'Hello World leek server';
+    ctx.body = 'Hello World leek server master';
 });
 
 app.listen(3002);
