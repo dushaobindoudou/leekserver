8567635ac35f5ad37f5fa3e966427c09f5a534e1
dushaobin <dushaobindoudou@gmail.com>
2018-10-24 13:56:11 +0800

    修改启动脚本

--- startLeekServer.sh
+++ startLeekServer.sh
@@ -6,6 +6,12 @@ sh /usr/mysql-app/startup.sh
 # start redis-server
 redis-server /etc/redis/redis.conf --daemonize yes
 
+# 初始化数据库
+yarn run mysql:init
+
 # 启动服务
 yarn run prod
+
+#pm2-runtime pm2.json
+#node src/index.js
 
